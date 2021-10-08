e663d775788c5d6e8998973b96073e1c8d7fc2a3
Karatus <shm1193@gmail.com>
2021-10-08 09:35:48 +0900

    Add hal
    implement rvpb uart
    feat
    - init
    - put/get

---
+++ hal/rvpb/Uart.c
@@ -0,0 +1,39 @@
+#include "Uart.h"
+#include "HalUart.h"
+#include "stdint.h"
+
+extern volatile PL011_t* Uart;
+
+void Hal_uart_init(void)
+{
+    // Enable UART
+    Uart->uartcr.bits.UARTEN = 0;
+    Uart->uartcr.bits.TXE = 1;
+    Uart->uartcr.bits.RXE = 1;
+    Uart->uartcr.bits.UARTEN = 1;
+}
+
+void Hal_uart_put_char(uint8_t ch)
+{
+    while (Uart->uartfr.bits.TXFF)
+        ;
+    Uart->uartdr.all = (ch & 0xff);
+}
+
+uint8_t Hal_uart_get_char(void)
+{
+    uint32_t data;
+
+    while (Uart->uartfr.bits.RXFE)
+        ;
+
+    data = Uart->uartdr.all;
+    //Check for an error flag
+    if (data & 0xffffff00) {
+        // Clear the error
+        Uart->uartrsr.all = 0xff;
+        return 0;
+    }
+
+    return (uint8_t)(data & 0xff);
+}
