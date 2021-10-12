07ecf4df2b6a24472eeb8773fc41899a625d5e8a
Karatus <shm1193@gmail.com>
2021-10-12 23:37:27 +0900

    update: add printf test

--- boot/Main.c
+++ boot/Main.c
@@ -1,9 +1,11 @@
 #include "stdint.h"
+#include "stdbool.h"
 #include "stdio.h"
 
 #include "HalUart.h"
 
 static void Hw_init(void);
+static void Printf_test(void);
 
 int main()
 {
@@ -15,14 +17,26 @@ int main()
     Hal_uart_put_char('\n');
     putstr("Hello World!\n");
 
-    i = 100;
-    while (i--) {
-        uint8_t ch = Hal_uart_get_char();
-        Hal_uart_put_char(ch);
-    }
+    Printf_test();
+
+    while (true);
 }
 
 static void Hw_init(void)
 {
     Hal_uart_init();
+}
+
+static void Printf_test(void)
+{
+    char *str = "printf pointer test";
+    char *nullptr = 0;
+    uint32_t i = 5;
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str);
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
+    debug_printf("print zero %u, overflow %u\n", 0, -1);
 }
